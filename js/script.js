f7fea24db955f85640f42dfab8525ef9d2cc33ec
Eugenum <sh_evgeniy.1996@mail.ru>
2022-08-25 21:19:42 +0300

    summ and commit

--- js/script.js
+++ js/script.js
@@ -1,28 +1,51 @@
 "use strict"
 
-const goods = [
-  { title: 'Shirt', price: 150, image: 'img/good_img.png' },
-  { title: 'Socks', price: 50, image: 'img/good_img.png' },
-  { title: 'Jacket', price: 350, image: 'img/good_img.png' },
-  { title: 'Shoes', price: 250, image: 'img/good_img.png' },
-  { title: 'Правый товар', price: 'Бесплатно', 
-    image:'img/depositphotos_10182313-stock-photo-hand-symbol.webp' },
-  ];
-
-const renderGoodsItem = (good) => {
-  return `<div class="goods-item">
-    <div class="goods__img-container">
-      <img src="${good.image}" class="goods__img">
-    </div>
-    <h3 class="goods__title">${good.title}</h3>
-    <p class="goods__price">${good.price}</p>
-    <button>Добавить</button>
-  </div>`;
-  };
-  const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item));
-  document.querySelector('.goods-list').innerHTML = goodsList.join('')
-  //Был ещё вариант про reduce, потом кааак понял про join
+class ProductsItem {
+    constructor(product) {
+    this.title = product.title;
+    this.id = product.id;
+    this.price = product.price;
+    this.image = product.image;
   }
-  renderGoodsList(goods);
-    
+  render() {
+    return `<div class="goods-item">
+      <div class="goods__img-container">
+        <img src="${this.image}" class="goods__img">
+      </div>
+      <h3 class="goods__title">${this.title}</h3>
+      <p class="goods__price">${this.price}</p>
+      <button>Добавить</button>
+    </div>`;
+  }
+}
+
+class ProductsList {
+  constructor(container = '.goods-list') {
+    this.goods = [];
+    this.container = container;
+    this._fetchProducts();
+    this.render()
+  }
+  _fetchProducts() {
+    this.goods = [
+      { title: 'Shirt', price: 150, image: 'img/good_img.png' },
+      { title: 'Socks', price: 50, image: 'img/good_img.png' },
+      { title: 'Jacket', price: 350, image: 'img/good_img.png' },
+      { title: 'Shoes', price: 250, image: 'img/good_img.png' },
+      { title: 'Правый товар', price: 0, 
+        image:'img/depositphotos_10182313-stock-photo-hand-symbol.webp' },
+      ];
+  }
+  render() {
+    const block = document.querySelector(this.container);
+    this.goods.forEach(good => {
+    const goodsItem = new ProductsItem(good);
+    block.insertAdjacentHTML('beforeend', goodsItem.render())
+    });
+  }
+  summAllGoods() {
+    return this.goods.reduce((summ, item) => summ + item.price, 0)
+  }
+}
+  let list = new ProductsList();
+  console.log(list.summAllGoods());
